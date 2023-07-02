75a11eeda8f5407967e6b77455f42c2d26d87992
James Yates <yates_7@hotmail.com>
2023-07-02 16:59:04 +0100

    Add documentation for the GroupArrayElements method

--- GroupArrayLibrary/GroupArray.cs
+++ GroupArrayLibrary/GroupArray.cs
@@ -2,7 +2,14 @@
 
 public static class GroupArray
 {
-    public static object[][] GroupArrayElements(this object[] input_array)
+    /* Returns the contents of the array divided into N equally sized arrays.
+    Where the size of the original array cannot be divided equally by N, the final
+    part has a length equal to the remainder.
+    The method takes the following arguments:
+    * input_array - array to be divided
+    * N - a positive integer indicating into how many parts the array should be split
+    */
+    public static object[][] GroupArrayElements(this object[] input_array, int n)
     {
         return new object[1][]{ input_array };
     }
