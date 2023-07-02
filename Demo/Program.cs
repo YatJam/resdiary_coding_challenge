5193d5372494308deb935e26a666591c8a115648
James Yates <yates_7@hotmail.com>
2023-07-02 15:37:22 +0100

    Add a demo console app

---
+++ Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using GroupArrayLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter a list of elements separated by spaces, e.g `1 2 3 abc");
+        string user_input = Console.ReadLine();
+        string[] elements = user_input.Split(" ");
+
+        var result = GroupArray.GroupArrayElements(elements);
+        Console.WriteLine(result.Length);
+    }
+}
+
--- GroupArrayLibrary/GroupArray.cs
+++ GroupArrayLibrary/GroupArray.cs
@@ -4,6 +4,6 @@ public static class GroupArray
 {
     public static object[][] GroupArrayElements(this object[] input_array)
     {
-        return new object[0][];
+        return new object[1][]{ input_array };
     }
 }
